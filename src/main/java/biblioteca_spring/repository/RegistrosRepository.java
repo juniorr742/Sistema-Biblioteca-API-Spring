8b6169dc97ebc106c60ad84bd97b146825e65842
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-04-27 14:17:36 -0300

    "feat: criando método de busca específico"

--- src/main/java/biblioteca_spring/repository/RegistrosRepository.java
+++ src/main/java/biblioteca_spring/repository/RegistrosRepository.java
@@ -3,5 +3,8 @@ package biblioteca_spring.repository;
 import biblioteca_spring.model.RegistroEmprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegistrosRepository extends JpaRepository<RegistroEmprestimo, Long> {
+    List<RegistroEmprestimo> findByUsuarioAndFinalizadoFalse(long idUsuario);
 }
--- src/main/java/biblioteca_spring/repository/UsuarioRepository.java
+++ src/main/java/biblioteca_spring/repository/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package biblioteca_spring.repository;
 
+import biblioteca_spring.model.Livro;
 import biblioteca_spring.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional <Usuario> findByNome (String nome);
 }
