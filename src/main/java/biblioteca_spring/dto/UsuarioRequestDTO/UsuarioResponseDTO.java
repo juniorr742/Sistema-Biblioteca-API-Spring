4385b7299df3b2fcedfb0efb1475b9a68717a80a
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-05-14 11:01:02 -0300

    "refactory: tirando classe e deixando record para ficar mais limpo"

--- src/main/java/biblioteca_spring/dto/UsuarioRequestDTO/UsuarioResponseDTO.java
+++ src/main/java/biblioteca_spring/dto/UsuarioRequestDTO/UsuarioResponseDTO.java
@@ -1,20 +1,4 @@
 package biblioteca_spring.dto.UsuarioRequestDTO;
 
-public class UsuarioResponseDTO {
-    private Long id;
-    private String nome;
-    private String email;
-    private String tipo;
-    private int limiteLivros;
-    private double limiteSaldo;
+    public record UsuarioResponseDTO(Long id, String nome, String email, String tipo, int limiteLivros, double limiteSaldo){}
 
-    public UsuarioResponseDTO(Long id, String nome, String email, String tipo, int limiteLivros, double limiteSaldo) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.tipo = tipo;
-        this.limiteLivros = limiteLivros;
-        this.limiteSaldo = limiteSaldo;
-    }
-
-}
