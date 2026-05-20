c587f4c26b66d7ae6028fe896e8100063f0dd264
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-05-20 09:54:07 -0300

    "feat: adicionando autorização bearer token no swagger"

--- src/main/java/biblioteca_spring/config/SwaggerConfig.java
+++ src/main/java/biblioteca_spring/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package biblioteca_spring.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI(){
         return new OpenAPI().info(new Info()
-                .title("Biblioteca API").version("1.0").description("API rest para gerenciamento de uma biblioteca com JWT"));
+                .title("Biblioteca API").version("1.0").description("API rest para gerenciamento de uma biblioteca com JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authetication")).components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
+
+
 }
