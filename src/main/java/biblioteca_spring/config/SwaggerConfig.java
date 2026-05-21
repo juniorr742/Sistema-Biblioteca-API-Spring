15c8f61db63d1402b9ce188933da7db545f61f39
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-05-21 10:04:43 -0300

    "fix: adiciona servidores https e http no swagger"

--- src/main/java/biblioteca_spring/config/SwaggerConfig.java
+++ src/main/java/biblioteca_spring/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package biblioteca_spring.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +9,8 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
@@ -15,8 +18,12 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI(){
         return new OpenAPI().info(new Info()
                 .title("Biblioteca API").version("1.0").description("API rest para gerenciamento de uma biblioteca com JWT"))
+                .servers(List.of(
+                        new Server().url("https://sistema-biblioteca-api-spring-production.up.railway.app").description("Servidor de Produção (Railway)"),
+                        new Server().url("http://localhost:8080").description("Servidor Local")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authetication")).components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT").in(SecurityScheme.In.HEADER).name("Authorization")));
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer").bearerFormat("JWT").in(SecurityScheme.In.HEADER).name("Authorization")));
     }
 
 
